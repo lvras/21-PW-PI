2f182357559965bc6ada3700bb5c5c5e55375fb4
lvras <lguerrero92@live.com>
2021-03-02 16:33:13 -0600

    Class for database connection

---
+++ Connection.php
@@ -0,0 +1,42 @@
+<?php
+
+class DbConnection
+{
+
+    public $host;
+    public $user;
+    public $password;
+    public $databaseName;
+    public $activeConnection;
+
+    public function __construct($host, $user, $password, $databaseName)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->databaseName = $databaseName;
+        $this->activeConnection = new mysqli($this->host, $this->user, $this->password, $this->databaseName);
+    }
+
+    /**
+     * Get an active mysql connection
+     */
+    public function getMySQLConnection()
+    {
+        if (!$this->activeConnection) {
+            $this->activeConnection = new mysqli($this->host, $this->user, $this->password, $this->databaseName);
+        }
+        if ($this->activeConnection->connect_errno) {
+            echo "Failed to connect to MySQL: " . $this->activeConnection->connect_error;
+            exit();
+        }
+        return $this->activeConnection;
+    }
+}
+
+
+
+// $db = new DbConnection('127.0.0.1', 'root', '', 'workshop7');
+// $query = "SELECT * FROM students LIMIT 2";
+// $result = $db->getMySQLConnection()->query($query);
+// print_r($result->fetch_all());
