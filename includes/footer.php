c5d9392441277d5a91e5cf94c1cc3415918cea7d
lvras <lguerrero92@live.com>
2021-03-03 22:48:59 -0600

    Design of the login page finished

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+  <!-- Scripts -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+</body>
+
+<footer class="bg-info text-center text-white mt-5">
+  <div class="container p-4 pb-0">
+    <section class="mb-2">
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-twitter"></i
+      ></a>
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fas fa-home"></i
+      ></a>
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-github"></i
+      ></a>
+    </section>
+  </div>
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2021 Copyright: lvras - Luis Guerrero
+  </div>
+</footer>
+</html>
--- login.php
+++ login.php
@@ -1,1 +1,33 @@
 <?php
+
+
+
+
+include("./includes/header.php");
+?>
+<section>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div class="card card-signin my-5">
+                    <div class="card-body">
+                        <h2 class="card-title text-center">Inicio de sesion</h2>
+                        <!-- <hr class="my-4"> -->
+                        <form class="form-signin mt-5" method="POST">
+                            <div class="form-label-group">
+                                <input type="email" name="email" class="form-control" placeholder="Email" required autofocus>
+                            </div>
+                            <div class="form-label-group mt-3">
+                                <input type="password" name="password" class="form-control" placeholder="Contraseña" required>
+                            </div>
+                            <button class="btn btn-lg btn-info btn-block text-uppercase mt-4" type="submit">Iniciar sesion</button>
+                            <hr class="my-4">
+                            <a href="#">No tienes cuenta? Create una ahora</a>
+                        </form>
+                    </div>
+                </div
+            </div>
+        </div>
+    </div>
+</section>
+<?php include("./includes/footer.php")?>
