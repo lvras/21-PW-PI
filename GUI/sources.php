5a919bbed52350ea7c63b855dbad8dccefcceca8
lvras <lguerrero92@live.com>
2021-03-04 00:43:26 -0600

    Design of the sources page finished

--- GUI/sources.php
+++ GUI/sources.php
@@ -1,10 +1,32 @@
 <?php
+
+
+
 include("../includes/header.php");
-
-
 ?>
+<div class="container mt-5">
+    <div class="row justify-content-around">
+        <div class="col-10">
+            <h2>News Sources</h2>
+            <hr class="my-4">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Categoria</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
 
-
-
-
+                ?>
+                </tbody>
+            </table>
+            <div class="col-3 justify-content-start pt-2">
+                <button class="btn btn-lg btn-info btn-block text-uppercase mt-5" type="submit">Agregar Nuevo</button>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include("../includes/footer.php")?>
