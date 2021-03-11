80f72a480c96a718e825c3883f840be2b9248813
lvras <lguerrero92@live.com>
2021-03-11 02:31:18 -0600

    Finished the function to update the news

--- DAO/updateNews.php
+++ DAO/updateNews.php
@@ -1,12 +1,16 @@
 <?php
-session_start();
+$conn = mysqli_connect('127.0.0.1', 'root', '', 'news_proyect');
+$queryN = "UPDATE news SET enabled = false";
+$resultN = mysqli_query($conn, $queryN);
 
-function ReedNews($feedURL, $id_news_source, $id_user, $id_category){
-    $conn = mysqli_connect('127.0.0.1', 'root', '', 'news_proyect');
-    $queryN = "UPDATE news SET enabled = false";
-    $resultN = mysqli_query($conn, $queryN);
+$queryNS = "SELECT * FROM news_sources WHERE enabled = true";
+$resultNS = mysqli_query($conn, $queryNS);
+while($row = mysqli_fetch_array($resultNS)){
     $i = 0;
-    $url = $feedURL;
+    $url = $row[1];
+    $id_news_source = $row[0];
+    $id_user = $row[5];
+    $id_category = $row[4];
     $rss = simplexml_load_file($url);
     foreach($rss->channel->item as $item) {
         $title = $item->title;  //extrae el titulo
@@ -18,6 +22,7 @@ function ReedNews($feedURL, $id_news_source, $id_user, $id_category){
         $stringCut = substr($description, 0, 200);
         $description = substr($stringCut, 0, strrpos($stringCut, ' ')).'...';
         $description = str_replace('"', " ", $description);
+        $description = str_replace("'", " ", $description);
         }
         if ($i < 10) { // extrae solo 10 items
             $query = "INSERT INTO news(title, short_description, permanlink, date, enabled, id_news_source, id_user, id_category) VALUES('$title', '$description', '$link', '$dateF', true, '$id_news_source', '$id_user', '$id_category')";
@@ -25,10 +30,8 @@ function ReedNews($feedURL, $id_news_source, $id_user, $id_category){
         }
         $i++;
     }
-    header("Location: ../GUI/sources.php");
 }
 
 function pubDateToMySql($str){
     return date('Y-m-d H:i:s', strtotime($str));
 };
-?>
